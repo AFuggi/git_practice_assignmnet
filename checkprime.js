76b09f3968a9da74493e2a267f1bdde9e68fb14c
AFuggi <gs15234@gmail.com>
2022-10-19 21:39:43 +0530

    checking num is prime or not

--- checkprime.js
+++ checkprime.js
@@ -5,7 +5,7 @@ for(let i=1; i<num; i++){
     if(num%i!==0){
         count++;
     }
-}if(count==2){
+}if(count==5){
     console.log(num," is Prime number");
 }else{
     console.log(num,"is not Prime ");
