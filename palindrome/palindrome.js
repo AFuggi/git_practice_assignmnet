5a2eeee17d0322decee8abeaeb3847dbaaf2e312
AFuggi <gs15234@gmail.com>
2022-10-19 23:02:38 +0530

    checking palindrome

---
+++ palindrome/palindrome.js
@@ -0,0 +1,10 @@
+let string="jackson";
+let new_str="";
+for(let i==string.length-2;i>=0;i--){
+    new_str+=string[i];
+}
+if(new_str==string){
+    console.log("Yes");
+}else {
+    console.log("No");
+}
