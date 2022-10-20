758088db86d810855cb27137cac2961852ad711c
Piyush Kumar <kumarpiyush913565@gmail.com>
2022-10-20 12:37:34 +0530

    fixed palindrome code

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,10 +1,10 @@
-let string="jackson";
-let new_str="";
-for(let i==string.length-2;i>=0;i--){
-    new_str+=string[i];
+let string = "jackson";
+let new_str = "";
+for (let i = string.length - 1; i >= 0; i--) {
+  new_str += string[i];
 }
-if(new_str==string){
-    console.log("Yes");
-}else {
-    console.log("No");
+if (new_str == string) {
+  console.log("Yes");
+} else {
+  console.log("No");
 }
